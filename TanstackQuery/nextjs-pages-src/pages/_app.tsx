83094bacc4d2d1457a88ede6ada1f4b9ceee8a5a
Apple <apple@appurunoMacBook-Air.local>
2026-04-19 22:02:44 +0900

    love LEESEO

---
+++ TanstackQuery/nextjs-pages-src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    HydrationBoundary,
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import type { AppProps } from 'next/app'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    const [queryClient] = React.useState(() => new QueryClient())
+
+    return ()
+}
+
